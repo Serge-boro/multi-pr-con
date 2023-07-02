51167677b458481a2bd456d9c5811d180e08a27d
Serge-boro <serhiiborodin83@gmail.com>
2023-07-02 09:55:21 -0400

    second

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import axios from '../node_modules/axios/dist/axios'
 const Fib = () => {
   const [isState, setIsState] = useState({
     values: {},
